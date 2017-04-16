2ae6656ac57489526454454881dba08c7b702ca1
KoikeTakayuki <tekasax@gmail.com>
2017-04-16 21:35:06 +0900

    Fixed exports

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = {
     Module: require('./core/module'),
     RecordField: require('./core/record-field'),
     ListField: require('./core/list-field'),
-    InnerRecordField: require('./core/inner-record-field'),
     NumberField: require('./core/number-field'),
     BooleanField: require('./core/boolean-field'),
     TextField: require('./core/text-field'),
