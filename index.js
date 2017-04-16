46da53b82906588cd96d6e8efc50a45c704ecd9c
KoikeTakayuki <tekasax@gmail.com>
2017-04-16 13:59:17 +0900

    Updated module.exports

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require('./system/core');
+const CoreModule = require('./core/core')
+
+module.exports = {
+    Trait: require('./core/trait'),
+    RecordType: require('./core/record-type'),
+    RecordField: require('./core/record-field'),
+    ListField: require('./core/list-field'),
+    NumberField: require('./core/number-field'),
+    BooleanField: require('./core/boolean-field'),
+    TextField: require('./core/text-field'),
+    TextType: CoreModule.TextType,
+    BooleanType: CoreModule.BooleanType,
+    NumberType: CoreModule.NumberType,
+};
