da87ae4c3ce2500652f1be4d53064fd373f455ec
小池 貴之 <d150024@doj.local>
2017-04-25 15:47:39 +0900

    Export record builders are now lower case

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const Types = require('./core/types');
 const Fields = require('./core/fields');
-const Trait = require('./core/trait');
-const RecordType = require('./core/record-type');
-const RecordField = require('./core/record-field');
-const ListField = require('./core/list-field');
-const NumberField = require('./core/number-field');
-const BooleanField = require('./core/boolean-field');
-const TextField = require('./core/text-field');
-const FunctionField = require('./core/function-field');
-const BinaryField = require('./core/binary-field');
+const trait = require('./core/trait');
+const recordType = require('./core/record-type');
+const recordField = require('./core/record-field');
+const listField = require('./core/list-field');
+const numberField = require('./core/number-field');
+const booleanField = require('./core/boolean-field');
+const textField = require('./core/text-field');
+const functionField = require('./core/function-field');
+const binaryField = require('./core/binary-field');
 
 module.exports = {
   Types,
   Fields,
-  Trait,
-  RecordType,
-  RecordField,
-  ListField,
-  NumberField,
-  BooleanField,
-  TextField,
-  FunctionField,
-  BinaryField,
+  trait,
+  recordType,
+  recordField,
+  listField,
+  numberField,
+  booleanField,
+  textField,
+  functionField,
+  binaryField,
 };
