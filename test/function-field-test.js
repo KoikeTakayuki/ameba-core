120fcad157b141eee62c7d2e1f104aab9db4e296
小池 貴之 <d150024@doj.local>
2017-04-25 16:40:24 +0900

    Create functionField test cases

---
+++ test/function-field-test.js
@@ -0,0 +1,61 @@
+const should = require('should');
+const functionField = require('../index').functionField;
+
+describe('functionField', function () {
+  it('should return an object', function () {
+    functionField('test').should.be.an.Object();
+  });
+
+  it('should return an object which has 6 keys', function () {
+    Object.keys(functionField('test')).should.have.length(6);
+  });
+
+  it('should return an object which has keys: [id, type, fieldType, isRequired, isListType, defautValue]', function () {
+    Object.keys(functionField('test')).should.have.length(6);
+  });
+
+  it('should return an object whose id is "test"', function () {
+    functionField('test').should.have.property('id', 'test');
+  });
+
+  it('should return an object whose type is an Object', function () {
+    functionField('test').type.should.be.an.Object();
+  });
+
+  it('should return an object whose type has id "Core.Field"', function () {
+    functionField('test').type.should.have.property('id', 'Core.Field');
+  });
+
+  it('should return an object whose fieldType is an Object', function () {
+    functionField('test').fieldType.should.be.an.Object();
+  });
+
+  it('should return an object whose fieldType has id "Core.FunctionType"', function () {
+    functionField('test').fieldType.should.have.property('id', 'Core.FunctionType');
+  });
+
+  it('should return an object whose isRequired is false if not specified', function () {
+    functionField('test').isRequired.should.false();
+  });
+
+  it('should return an object whose isRequired is true if specified', function () {
+    functionField('test', { isRequired: true }).isRequired.should.true();
+  });
+
+  it('should return an object whose isListField is false if not specified', function () {
+    functionField('test').isListField.should.false();
+  });
+
+  it('should return an object whose isListField is false even if specified', function () {
+    functionField('test', { isListField: true }).isListField.should.false();
+  });
+
+  it('should return an object whose defaultValue is undefined if not specified', function () {
+    should.equal(functionField('test').defaultValue, undefined);
+  });
+
+  it('should return an object whose defaultValue is specified value', function () {
+    const testFunction = x => x;
+    functionField('test', { defaultValue: testFunction }).defaultValue.should.be.equal(testFunction);
+  });
+});
