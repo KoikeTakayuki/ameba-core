5c71a853f86fff1e639014dea43cf188f7a0252f
小池 貴之 <d150024@doj.local>
2017-04-25 16:24:02 +0900

    Create numberField test cases

---
+++ test/number-field-test.js
@@ -0,0 +1,60 @@
+const should = require('should');
+const numberField = require('../index').numberField;
+
+describe('numberField', function () {
+  it('should return an object', function () {
+    numberField('test').should.be.an.Object();
+  });
+
+  it('should return an object which has 6 keys', function () {
+    Object.keys(numberField('test')).should.have.length(6);
+  });
+
+  it('should return an object which has keys: [id, type, fieldType, isRequired, isListType, defautValue]', function () {
+    Object.keys(numberField('test')).should.have.length(6);
+  });
+
+  it('should return an object whose id is "test"', function () {
+    numberField('test').should.have.property('id', 'test');
+  });
+
+  it('should return an object whose type is an Object', function () {
+    numberField('test').type.should.be.an.Object();
+  });
+
+  it('should return an object whose type has id "Core.Field"', function () {
+    numberField('test').type.should.have.property('id', 'Core.Field');
+  });
+
+  it('should return an object whose fieldType is an Object', function () {
+    numberField('test').fieldType.should.be.an.Object();
+  });
+
+  it('should return an object whose fieldType has id "Core.NumberType"', function () {
+    numberField('test').fieldType.should.have.property('id', 'Core.NumberType');
+  });
+
+  it('should return an object whose isRequired is false if not specified', function () {
+    numberField('test').isRequired.should.false();
+  });
+
+  it('should return an object whose isRequired is true if specified', function () {
+    numberField('test', { isRequired: true }).isRequired.should.true();
+  });
+
+  it('should return an object whose isListField is false if not specified', function () {
+    numberField('test').isListField.should.false();
+  });
+
+  it('should return an object whose isListField is false even if specified', function () {
+    numberField('test', { isListField: true }).isListField.should.false();
+  });
+
+  it('should return an object whose defaultValue is undefined if not specified', function () {
+    should.equal(numberField('test').defaultValue, undefined);
+  });
+
+  it('should return an object whose defaultValue is specified value', function () {
+    numberField('test', { defaultValue: 0 }).defaultValue.should.be.equal(0);
+  });
+});
