d94d8cc51dc2699e949f302e1c412c132f49cc17
koiketakayuki <koike.takayuki0907@gmail.com>
2017-05-22 20:17:28 +0900

    Update exports

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const enumerationType = require('./core/enumeration-type');
 const recordField = require('./core/record-field');
 const listField = require('./core/list-field');
 const numberField = require('./core/number-field');
+const intField = require('./core/int-field');
 const booleanField = require('./core/boolean-field');
 const dateField = require('./core/date-field');
+const timeField = require('./core/time-field');
+const dateTimeField = require('./core/date-time-field');
 const textField = require('./core/text-field');
+const longTextField = require('./core/long-text-field');
+const richTextField = require('./core/rich-text-field');
 const functionField = require('./core/function-field');
 const binaryField = require('./core/binary-field');
 
@@ -21,9 +26,14 @@ module.exports = {
   recordField,
   listField,
   numberField,
+  intField,
   booleanField,
   dateField,
+  timeField,
+  dateTimeField,
   textField,
+  longTextField,
+  richTextField,
   functionField,
   binaryField,
 };
