0fd538ee10f93e104d5014d5e6a4dbca1a3dc3db
koiketakayuki <koike.takayuki0907@gmail.com>
2017-07-14 00:03:46 +0900

    Add test case for an original record type

--- test/util/getRootType_spec.js
+++ test/util/getRootType_spec.js
@@ -1,6 +1,7 @@
 const should = require('should');
 const getRootType = require('../../').util.getRootType;
 const core = require('../../').core;
+const recordType = require('../../').recordType;
 
 describe('getRootType', function () {
   it('should return Record when Record given', function () {
@@ -13,5 +14,12 @@ describe('getRootType', function () {
 
   it('should return Trait when RecordType given', function () {
     getRootType(core.RecordType).should.be.equal(core.Trait);
+  });
+
+  it('should return all fields of given recordType, its traits and baseType ', function () {
+    const testRecordType1 = recordType('a', 'a', []);
+    const testRecordType2 = recordType('b', 'b', [], { baseType: testRecordType1 });
+    const testRecordType3 = recordType('c', 'c', [], { baseType: testRecordType2 });
+    getRootType(testRecordType3).should.be.equal(testRecordType1);
   });
 });
