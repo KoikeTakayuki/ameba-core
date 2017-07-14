69e4dd79df768641411e1cd809da1d3172f0f2db
koiketakayuki <koike.takayuki0907@gmail.com>
2017-07-14 13:24:03 +0900

    Create trait_spec.js

---
+++ test/trait_spec.js
@@ -0,0 +1,38 @@
+const should = require('should');
+const Trait = require('../').core.Trait;
+const trait = require('../').trait;
+const textField = require('../').field.textField;
+
+const testId = 'testId';
+const testName = 'testName';
+const testFields = [textField('test', 'test')];
+
+describe('getHierarchyFields', function () {
+  it('should return an object', function () {
+    trait(testId, testName, testFields).should.be.an.Object();
+  });
+
+  it('should return an object which has 4 keys', function () {
+    Object.keys(trait(testId, testName, testFields)).should.have.length(4);
+  });
+
+  it('should return an object whose keys are ["type", "id", "name", "fields"]', function () {
+    trait(testId, testName, testFields).should.have.keys('type', 'id', 'name', 'fields');
+  });
+
+  it('should return an object whose type is Trait', function () {
+    trait(testId, testName, testFields).should.have.property('type', Trait);
+  });
+
+  it('should return an object whose id is given id', function () {
+    trait(testId, testName, testFields).should.have.property('id', testId);
+  });
+
+  it('should return an object whose name is given id', function () {
+    trait(testId, testName, testFields).should.have.property('name', testName);
+  });
+
+  it('should return an object whose fields are given fields', function () {
+    trait(testId, testName, testFields).should.have.property('fields', testFields);
+  });
+});
