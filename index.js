cf8d122e08b86621f06b915bfcc8917a24dae557
koiketakayuki <koike.takayuki0907@gmail.com>
2017-07-14 14:53:18 +0900

    Export record

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const core = require('./src/core');
 const util = require('./src/util');
 const field = require('./src/field');
+const record = require('./src/record');
 const recordType = require('./src/recordType');
 const trait = require('./src/trait');
 
 module.exports = {
   core,
   util,
+  field,
+  record,
   recordType,
   trait,
-  field,
 };
